693d0bb22d417c2f9326f1ee75b83cfadd7f1b3e
fluke <devboxever17@gmail.com>
2022-11-07 19:18:52 -0300

    fix width widget

--- lib/app/inserts/widgets/button_with_icon_widget.dart
+++ lib/app/inserts/widgets/button_with_icon_widget.dart
@@ -28,7 +28,7 @@ class ButtonWithIconWidget extends StatelessWidget {
       ),
       child: Container(
         height: 50,
-        width: 135,
+        width: 140,
         decoration: BoxDecoration(
           color: colorButton,
           borderRadius: BorderRadius.circular(
